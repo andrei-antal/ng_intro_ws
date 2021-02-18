9759ed14aed39ee306bcb151f0a86d7151650064
Andrei Antal <antal.andrei@icloud.com>
2021-02-18 14:05:20 +0200

    done challenge 2

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MovieItemComponent } from './components/movie-item/movie-item.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [MovieItemComponent],
+  exports: [MovieItemComponent],
 })
 export class MoviesModule {}
