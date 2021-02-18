b656e32e61d0e454b4fdb5689dcfba59b8d094b9
Andrei Antal <antal.andrei@icloud.com>
2021-02-18 15:30:52 +0200

    done challenge 9

--- src/app/movies/components/movie-item/movie-item.component.ts
+++ src/app/movies/components/movie-item/movie-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../model/movie';
 
 @Component({
   selector: 'ngm-movie-item',
@@ -6,56 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./movie-item.component.scss'],
 })
 export class MovieItemComponent {
-  public movies = [
-    {
-      id: 1,
-      title: 'Star Wars Episode IX: The Rise of Skywalker',
-      year: 2019,
-      genre: 'Action, Adventure, Fantasy',
-      plot:
-        'The surviving Resistance faces the First Order once more in the final chapter of the Skywalker saga.',
-      poster:
-        'https://images-na.ssl-images-amazon.com/images/I/91rKEgY1qDL._SY679_@@._V1_SX300.jpg',
-      comment: '',
-      commentSaved: false,
-    },
-    {
-      id: 2,
-      title: 'Avengers: Endgame',
-      year: 2019,
-      genre: 'Action, Adventure, Drama',
-      plot:
-        "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
-      poster:
-        'https://images-na.ssl-images-amazon.com/images/I/81%2BNup8-8NL._SY445_@@._V1_SX300.jpg',
-      comment: '',
-      commentSaved: false,
-    },
-    {
-      id: 3,
-      title: 'Terminator: Dark Fate',
-      year: 2019,
-      genre: 'Action, Adventure, Sci-Fi',
-      plot:
-        "An augmented human and Sarah Connor must stop an advanced liquid Terminator, from hunting down a young girl, who's fate is critical to the human race.",
-      poster:
-        'https://images-na.ssl-images-amazon.com/images/I/911bTXwO--L._SL1500_@@._V1_SX300.jpg',
-      comment: '',
-      commentSaved: false,
-    },
-    {
-      id: 4,
-      title: 'John Wick: Chapter 3 - Parabellum',
-      year: 2019,
-      genre: 'Action, Crime, Thriller',
-      plot:
-        "John Wick is on the run after killing a member of the international assassin's guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.",
-      poster:
-        'https://images-na.ssl-images-amazon.com/images/I/91XKS5MN9uL._SY741_@@._V1_SX300.jpg',
-      comment: '',
-      commentSaved: false,
-    },
-  ];
+  @Input() public movie: Movie;
+  public commentSaved: boolean;
 
   public wordCount(comment: string): number {
     if (!comment || comment.length === 0) {
@@ -65,18 +18,14 @@ export class MovieItemComponent {
     }
   }
 
-  public saveComment(id: number): void {
-    const theMovie = this.movies.find((movie) => movie.id === id);
-    theMovie.commentSaved = !theMovie.commentSaved;
+  public saveComment(id: string): void {
+    // const theMovie = this.movies.find((movie) => movie.id === id);
+    // theMovie.commentSaved = !theMovie.commentSaved;
   }
 
-  public clearComment(id: number): void {
-    const theMovie = this.movies.find((movie) => movie.id === id);
-    theMovie.comment = '';
-    theMovie.commentSaved = false;
-  }
-
-  public trackByFn(_: any, movie): number {
-    return movie.id;
+  public clearComment(id: string): void {
+    // const theMovie = this.movies.find((movie) => movie.id === id);
+    // theMovie.comment = '';
+    // theMovie.commentSaved = false;
   }
 }
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
 import { FormsModule } from '@angular/forms';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [MovieItemComponent],
-  exports: [MovieItemComponent],
+  declarations: [MovieItemComponent, MovieListComponent],
+  exports: [MovieListComponent],
 })
 export class MoviesModule {}
