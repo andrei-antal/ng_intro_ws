f70d771b87d3fd7b6a340c7fb31827117f314dbc
Andrei Antal <antal.andrei@icloud.com>
2021-02-18 14:37:43 +0200

    done challenge 3

--- src/app/movies/components/movie-item/movie-item.component.ts
+++ src/app/movies/components/movie-item/movie-item.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './movie-item.component.html',
   styleUrls: ['./movie-item.component.scss'],
 })
-export class MovieItemComponent {}
+export class MovieItemComponent {
+  public title = 'Star Wars Episode IX: The Rise of Skywalker';
+  public year = 2019;
+  public genre = 'Action, Adventure, Fantasy';
+  public plot =
+    'The surviving Resistance faces the First Order once more in the final chapter of the Skywalker saga.';
+  public poster =
+    'https://images-na.ssl-images-amazon.com/images/I/91rKEgY1qDL._SY679_@@._V1_SX300.jpg';
+}
