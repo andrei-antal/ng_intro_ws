7ed6f06b5074d7f3c4c7ea519a86ff8f0afa0808
Andrei Antal <antal.andrei@icloud.com>
2021-03-11 19:23:52 +0200

    done challenge 25

--- src/app/movies/services/movies-validators.service.spec.ts
+++ src/app/movies/services/movies-validators.service.spec.ts
@@ -1,7 +1,38 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { MoviesValidatorsService } from './movies-validators.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MovieService } from './movie.service';
+import { FormControl } from '@angular/forms';
+
+describe('MoviesValidatorsService - static', () => {
+  it('should validate genre', () => {
+    // Arrange
+    const validator = new MoviesValidatorsService({} as MovieService);
+    const fc = new FormControl();
+    fc.setValidators(validator.genreSync);
+
+    // Act
+    fc.setValue('action, thriller');
+    // Assert
+    expect(fc.status).toBe('VALID');
+
+    // Act
+    fc.setValue('comedy');
+    // Assert
+    expect(fc.status).toBe('VALID');
+
+    // Act
+    fc.setValue('action, thrlr');
+    // Assert
+    expect(fc.status).toBe('INVALID');
+
+    // Act
+    fc.setValue('cmdy');
+    // Assert
+    expect(fc.status).toBe('INVALID');
+  });
+});
 
 describe('MoviesValidatorsService', () => {
   let service: MoviesValidatorsService;
