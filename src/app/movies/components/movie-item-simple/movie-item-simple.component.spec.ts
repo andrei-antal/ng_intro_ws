96eecbc1bd70d2622f8ada6c622c608d5ebc297f
Andrei Antal <antal.andrei@icloud.com>
2021-03-11 19:31:43 +0200

    done chalenge 26

--- src/app/movies/components/movie-item-simple/movie-item-simple.component.spec.ts
+++ src/app/movies/components/movie-item-simple/movie-item-simple.component.spec.ts
@@ -3,6 +3,7 @@ import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MovieItemSimpleComponent } from './movie-item-simple.component';
 import { DebugElement } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('MovieItemSimpleComponent', () => {
   let component: MovieItemSimpleComponent;
@@ -35,5 +36,42 @@ describe('MovieItemSimpleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should correctly display the movie title', () => {
+    // Arrange + Act
+    const titleElement: HTMLHeadingElement = movieItemDe.query(
+      By.css('[data-testId=title]')
+    ).nativeElement;
+    // Assert
+    expect(titleElement.textContent).toContain(
+      'Star Wars Episode IX: The Rise of Skywalker'
+    );
+  });
+
+  it('should correctly change word count color', () => {
+    // Arrange
+    wordsElement = movieItemDe.query(By.css('[data-testId=words]'))
+      .nativeElement;
+    commentElement = movieItemDe.query(By.css('[data-testId=comment]'))
+      .nativeElement;
+    expect(wordsElement.style.color).toBe('darkred');
+
+    // Act
+    commentElement.value = 'Comment';
+    commentElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    // Assert
+
+    expect(wordsElement.style.color).toContain('darkgreen');
+
+    // Act
+    commentElement.value = '';
+    commentElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+
+    // Assert
+    expect(wordsElement.style.color).toContain('darkred');
   });
 });
